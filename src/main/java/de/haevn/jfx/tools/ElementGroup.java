9c8e5fdad243aeddda911dc4fe2abcd8714879db
Nils M <nils.milewski@stud.hs-ruhrwest.de>
2024-02-13 16:33:38 +0100

    Relocated couple classes Deprecated Core class Introduced new separated classes for core functionality Added Grouping of common elements

--- src/main/java/de/haevn/jfx/tools/ElementGroup.java
+++ src/main/java/de/haevn/jfx/tools/ElementGroup.java
@@ -1,8 +1,12 @@
 package de.haevn.jfx.tools;
 
 
-import de.haevn.jfx.elements.menu.SliderMenu;
-import javafx.scene.control.Control;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Region;
 
 import java.util.ArrayList;
@@ -15,52 +19,253 @@ public class ElementGroup {
     public ElementGroup() {
     }
 
-    public ElementGroup(Control... nodes) {
+    public ElementGroup(final Region... nodes) {
         Collections.addAll(this.nodes, nodes);
     }
 
-    public ElementGroup add(Control node) {
+    public ElementGroup add(final Region node) {
         nodes.add(node);
         return this;
     }
 
-    public ElementGroup addAll(Control... nodes) {
+    public ElementGroup addAll(final Region... nodes) {
         Collections.addAll(this.nodes, nodes);
         return this;
     }
 
-    public ElementGroup remove(Control node) {
+    public ElementGroup remove(final Region node) {
         nodes.remove(node);
         return this;
     }
 
-    public ElementGroup setEnabled(boolean enabled) {
+    public ElementGroup setEnabled(final boolean enabled) {
         nodes.forEach(node -> node.setDisable(!enabled));
         return this;
     }
 
-    public ElementGroup setVisible(boolean visible) {
+    public ElementGroup setVisible(final boolean visible) {
         nodes.forEach(node -> node.setVisible(visible));
         return this;
     }
 
-    public ElementGroup setStyle(String style) {
+    public ElementGroup setStyle(final String style) {
         nodes.forEach(node -> node.setStyle(style));
         return this;
     }
 
-    public ElementGroup setStyleClass(String styleClass) {
+    public ElementGroup setStyleClass(final String styleClass) {
         nodes.forEach(node -> node.getStyleClass().add(styleClass));
         return this;
     }
 
-    public ElementGroup setStyleClass(String... styleClasses) {
+    public ElementGroup setStyleClass(final String... styleClasses) {
         nodes.forEach(node -> Collections.addAll(node.getStyleClass(), styleClasses));
+        return this;
+    }
+
+    public ElementGroup setPadding(final double padding) {
+        nodes.forEach(node -> node.setPadding(new Insets(padding)));
+        return this;
+    }
+
+    public ElementGroup setPadding(final double top, final double right, final double bottom, final double left) {
+        nodes.forEach(node -> node.setPadding(new Insets(top, right, bottom, left)));
+        return this;
+    }
+
+    public ElementGroup setMaxWidth(final double width) {
+        nodes.forEach(node -> node.setMaxWidth(width));
+        return this;
+    }
+
+    public ElementGroup setMinWidth(final double width) {
+        nodes.forEach(node -> node.setMinWidth(width));
+        return this;
+    }
+
+    public ElementGroup setPrefWidth(final double width) {
+        nodes.forEach(node -> node.setPrefWidth(width));
+        return this;
+    }
+
+    public ElementGroup setFixedWidth(final double width) {
+        nodes.forEach(node -> {
+            node.setMinWidth(width);
+            node.setMaxWidth(width);
+            node.setPrefWidth(width);
+        });
+        return this;
+    }
+
+    public ElementGroup setMaxHeight(final double height) {
+        nodes.forEach(node -> node.setMaxHeight(height));
+        return this;
+    }
+
+    public ElementGroup setMinHeight(final double height) {
+        nodes.forEach(node -> node.setMinHeight(height));
+        return this;
+    }
+
+    public ElementGroup setPrefHeight(final double height) {
+        nodes.forEach(node -> node.setPrefHeight(height));
+        return this;
+    }
+
+    public ElementGroup setFixedHeight(final double height) {
+        nodes.forEach(node -> {
+            node.setMinHeight(height);
+            node.setMaxHeight(height);
+            node.setPrefHeight(height);
+        });
+        return this;
+    }
+
+    public ElementGroup setOnDragDetected(final EventHandler<? super MouseEvent> eventHandler) {
+        nodes.forEach(node -> node.setOnDragDetected(eventHandler));
+        return this;
+    }
+
+    public ElementGroup setOnDragDone(final EventHandler<? super DragEvent> eventHandler) {
+        nodes.forEach(node -> node.setOnDragDone(eventHandler));
+        return this;
+    }
+
+    public ElementGroup setOnDragEntered(final EventHandler<? super DragEvent> eventHandler) {
+        nodes.forEach(node -> node.setOnDragEntered(eventHandler));
+        return this;
+    }
+
+    public ElementGroup setOnDragExited(final EventHandler<? super DragEvent> eventHandler) {
+        nodes.forEach(node -> node.setOnDragExited(eventHandler));
+        return this;
+    }
+
+    public ElementGroup setOnDragOver(final EventHandler<? super DragEvent> eventHandler) {
+        nodes.forEach(node -> node.setOnDragOver(eventHandler));
+        return this;
+    }
+
+    public ElementGroup setOnDragDropped(final EventHandler<? super DragEvent> eventHandler) {
+        nodes.forEach(node -> node.setOnDragDropped(eventHandler));
+        return this;
+    }
+
+    public ElementGroup setOnMouseDragEntered(final EventHandler<? super MouseEvent> eventHandler) {
+        nodes.forEach(node -> node.setOnMouseDragEntered(eventHandler));
+        return this;
+    }
+
+    public ElementGroup setOnMouseDragExited(final EventHandler<? super MouseEvent> eventHandler) {
+        nodes.forEach(node -> node.setOnMouseDragExited(eventHandler));
+        return this;
+    }
+
+    public ElementGroup setOnMouseDragOver(final EventHandler<? super MouseEvent> eventHandler) {
+        nodes.forEach(node -> node.setOnMouseDragOver(eventHandler));
+        return this;
+    }
+
+    public ElementGroup setOnMouseDragReleased(final EventHandler<? super MouseEvent> eventHandler) {
+        nodes.forEach(node -> node.setOnMouseDragReleased(eventHandler));
+        return this;
+    }
+
+    public ElementGroup setOnMouseDragged(final EventHandler<? super MouseEvent> eventHandler) {
+        nodes.forEach(node -> node.setOnMouseDragged(eventHandler));
+        return this;
+    }
+
+
+    public ElementGroup setSnapToPixel(final boolean snapToPixel) {
+        nodes.forEach(node -> node.setSnapToPixel(snapToPixel));
+        return this;
+    }
+
+    public ElementGroup setOnKeyPressed(final EventHandler<? super KeyEvent> eventHandler) {
+        nodes.forEach(node -> node.setOnKeyPressed(eventHandler));
+        return this;
+    }
+
+    public ElementGroup setOnKeyReleased(final EventHandler<? super KeyEvent> eventHandler) {
+        nodes.forEach(node -> node.setOnKeyReleased(eventHandler));
+        return this;
+    }
+
+    public ElementGroup setOnKeyTyped(final EventHandler<? super KeyEvent> eventHandler) {
+        nodes.forEach(node -> node.setOnKeyTyped(eventHandler));
+        return this;
+    }
+
+    public ElementGroup setOnMouseClicked(final EventHandler<? super MouseEvent> eventHandler) {
+        nodes.forEach(node -> node.setOnMouseClicked(eventHandler));
+        return this;
+    }
+
+    public ElementGroup setOnMouseEntered(final EventHandler<? super MouseEvent> eventHandler) {
+        nodes.forEach(node -> node.setOnMouseEntered(eventHandler));
+        return this;
+    }
+
+    public ElementGroup setOnMouseExited(final EventHandler<? super MouseEvent> eventHandler) {
+        nodes.forEach(node -> node.setOnMouseExited(eventHandler));
+        return this;
+    }
+
+    public ElementGroup setOnMouseMoved(final EventHandler<? super MouseEvent> eventHandler) {
+        nodes.forEach(node -> node.setOnMouseMoved(eventHandler));
+        return this;
+    }
+
+    public ElementGroup setOnMousePressed(final EventHandler<? super MouseEvent> eventHandler) {
+        nodes.forEach(node -> node.setOnMousePressed(eventHandler));
+        return this;
+    }
+
+    public ElementGroup setOnMouseReleased(final EventHandler<? super MouseEvent> eventHandler) {
+        nodes.forEach(node -> node.setOnMouseReleased(eventHandler));
+        return this;
+    }
+
+    public ElementGroup setOnMouseLeftButtonClicked(final Runnable runnable) {
+        nodes.forEach(node -> node.setOnMouseClicked(event -> {
+            if (event.isPrimaryButtonDown()) {
+                runnable.run();
+            }
+        }));
+        return this;
+    }
+
+    public ElementGroup setOnMouseRightButtonClicked(final Runnable runnable) {
+        nodes.forEach(node -> node.setOnMouseClicked(event -> {
+            if (event.isSecondaryButtonDown()) {
+                runnable.run();
+            }
+        }));
+        return this;
+    }
+
+    public ElementGroup setOnMouseMiddleButtonClicked(final Runnable runnable) {
+        nodes.forEach(node -> node.setOnMouseClicked(event -> {
+            if (event.isMiddleButtonDown()) {
+                runnable.run();
+            }
+        }));
+        return this;
+    }
+
+    public ElementGroup setOnKeyPressed(final Runnable runnable, final KeyCode keyCode) {
+        nodes.forEach(node -> node.setOnKeyPressed(event -> {
+            if (event.getCode() == keyCode) {
+                runnable.run();
+            }
+        }));
         return this;
     }
 
 
     public ElementGroup enable() {
+        Region region = new Region();
         return setEnabled(true);
     }
 
@@ -77,10 +282,8 @@ public class ElementGroup {
     }
 
 
-
-
-
     public List<Region> getElements() {
         return nodes;
     }
+
 }
